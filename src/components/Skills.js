bf834b08f115f4954a5d7596ddda41e38ac6ad67
Sergio Junca <sergio.junca@gmail.com>
2018-06-20 23:26:33 -0500

    deployment backup

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,8 +8,6 @@ class Skills extends Component {
                 <Cell col={12}>
                     <div style={{display:"flex"}}>
                     {this.props.skill} <ProgressBar style={{margin: "auto", width: "75%"}} progress={this.props.progress} />
-                        
-
                     </div>
                 </Cell>
             </Grid>
